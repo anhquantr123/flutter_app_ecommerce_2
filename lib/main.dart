6e3ca4d5e736d96d3a5dcbd0ee85403ffd1e4002
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-09-25 15:46:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_ecommerce_2/screens/constants/screen_export.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/screens/constants/constants_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+// export style use our app
+const primaryColor = Color(0xFFF58A5F);
+const whiteColor = Color(0xFFFFFFFF);
+const gradientColor = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [Color(0xFFFFA53E), Color(0xFFFF7643)]);
+
+const textColorBlack = Color(0xFF000000);
+const textColorWhite = Color(0xFFFFFFFF);
+const textSizeHeading = 20.0;
+const textSizeBody = 16.0;
+const fontBold = FontWeight.bold;
+const fontNormal = FontWeight.normal;
