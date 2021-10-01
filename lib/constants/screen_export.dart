7048f8d6c1fc67e55ac44080037c11a2bbebc03f
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-10-01 21:46:48 +0700

    add sign up

--- lib/constants/screen_export.dart
+++ lib/constants/screen_export.dart
@@ -1,2 +1,3 @@
 export '../screens/splash/splash_screen.dart';
 export '../screens/sign_in/sign_in_screen.dart';
+export '../screens/sign_in/sign_up_screen.dart';
---
+++ lib/screens/sign_in/sign_up_screen.dart
@@ -0,0 +1,8 @@
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
